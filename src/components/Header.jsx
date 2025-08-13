5707751ec01285e7804f33d2adfd2d71f5f23ff2
Jiten Kumar <work.jiten282003@gmail.com>
2025-08-13 12:55:58 +0530

    Implement code changes to enhance functionality and improve performance

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,15 @@
-import BizlinkLogo from '/src/assets/BizLink Logo.png';
+import darkLogo from '/src/assets/BizLink Logo.png';
+import lightLogo from '/src/assets/BizLink Logo Dark.png';
 
 export default function Header({ theme, setTheme }) {
     return (
     <header className="flex items-center justify-between p-4 md:px-8 bg-white/60 dark:bg-gray-900/60 backdrop-blur sticky top-0 z-20">
         <div className="flex items-center gap-3">
-        <div className="w-10 h-10 rounded-full bg-transparent flex items-center justify-center">
-            <img
-                src={BizlinkLogo}
-                alt="BizLink Logo"
-                className="w-10 h-10 rounded-full"
-            />
-        </div>
+        <img
+            src={theme === "dark" ? darkLogo : lightLogo}
+            alt="BizLink logo"
+            className="w-10 h-10 rounded-full"
+        />
         <div>
             <h1 className="text-lg font-semibold">BizLink</h1>
             <p className="text-sm text-gray-500 dark:text-gray-400">
